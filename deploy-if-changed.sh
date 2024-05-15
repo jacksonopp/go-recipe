9c5f05ab21dbe1194c24cf3c58a3ee3e6f87b72d
Jackson Oppenheim <50721527+jacksonopp@users.noreply.github.com>
2024-05-15 10:22:10 -0500

    add deploy scripts

---
+++ deploy-if-changed.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "$(date --utc +%FT%TZ): Fetching remote repository..."
+git fetch
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+	echo "$(date --utc +%FT%TZ): No changeds detected in git"
+elif [ $LOCAL = $BASE ]; then
+	BUILD_VERSION=$(git rev-parse HEAD)
+	echo "$(date --utc +%FT%TZ): Changes detected, deploying new version: $BUILD_VERSION"
+elif [ $REMOTE = $BASE ]; then
+	echo "$(date --utc +%FT%TZ): Local changes detected, stashing"
+	git stash
+	./deploy.sh
+else
+	echo "$(date --utc +%FT%TZ): Git is diverged, this is unexpected."
+fi
---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+git pull
+
+BUILD_VERSION=$(git rev-parse HEAD)
+
+:echo "$(date --utc +%FT%TZ) Releaseing new server version. $BUILD_VERSION"
+
+echo "$(date --utc +%FT%TZ): Running build..."
+docker compose rm -f
+docker compose build
+
+OLD_CONTAINER=$(docker ps -aqf "name=server")
+echo "$(date --utc +%FT%TZ): Scaling server up..."
+BUILD_VERSION=$BUILD_VERSION docker compose up -d --no-deps --scale server=2 --no-recreate server
+
+sleep 30
+
+echo "$(date --utc +%FT%TZ): Scaling old server down..."
+docker container rm -f $OLD_CONTAINER
+docker compose up -d --no-deps --scale server=1 --no-recreate server
