24661f7320b3c164c2d8890e32c3e249d9399105
Jackson Oppenheim <50721527+jacksonopp@users.noreply.github.com>
2024-05-20 20:17:26 -0500

    join form validation

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,11 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
       tailwindcss: {},
-        autoprefixer: {},
-    }
-  }
-})
+      autoprefixer: {},
+    },
+  },
+  routeRules: {
+    "/api/**": { proxy: "http://127.0.0.1:8080/api/**" },
+  },
+});
--- client/pages/join.vue
+++ client/pages/join.vue
@@ -1,14 +1,44 @@
 <script setup lang="ts">
-const formData = ref({
-  name: "",
-  email: "",
-  password: "",
-  passwordConfirm: "",
+const fd = {
+  username: { value: "", dirty: false },
+  // email: { value: "", dirty: false },
+  password: { value: "", dirty: false },
+  passwordConfirm: { value: "", dirty: false },
+};
+
+type FdKeys = keyof typeof fd;
+
+const formData = reactive(fd);
+
+const markDirty = (field: FdKeys) => {
+  console.log("###", field);
+  formData[field].dirty = true;
+};
+
+const isUsernameValid = computed(() => {
+  const pattern = /^[a-zA-Z0-9]{3,20}$/;
+
+  // Test the username against the pattern
+  return pattern.test(formData.username.value);
 });
 
-const submitForm = () => {
-  console.log(formData.value);
-};
+const isPasswordLongEnough = computed(() => {
+  return formData.password.value.length >= 8;
+});
+
+const doPasswordsMatch = computed(() => {
+  return formData.password.value === formData.passwordConfirm.value;
+});
+
+const buttonDisabled = computed(() => {
+  return (
+    !isPasswordLongEnough.value ||
+    !doPasswordsMatch.value ||
+    !isUsernameValid.value
+  );
+});
+
+const submitForm = () => {};
 </script>
 
 <template>
@@ -27,46 +57,64 @@ const submitForm = () => {
           </p>
           <form v-on:submit.prevent="submitForm">
             <div class="flex flex-col gap-4 md:flex-row">
-              <div class="flex w-full flex-col">
-                <label for="name" class="my-2 text-sm text-white">Name</label>
+              <!--name formgroup-->
+              <div class="w-full">
+                <label for="name" class="my-2 text-sm text-white"
+                  >Username</label
+                >
                 <input
-                  v-model="formData.name"
+                  @input="() => markDirty('username')"
+                  v-model="formData.username.value"
                   class="flex h-10 w-full flex-1 rounded-md file:border-0 border file:bg-transparent bg-white/20 px-4 py-3 text-sm file:text-sm file:font-medium text-white placeholder:text-gray-200 border-input ring-offset-background focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 focus-visible:ring-ring focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                   type="text"
                 />
-              </div>
-              <div class="flex w-full flex-col">
-                <label for="name" class="my-2 text-sm text-white">Name</label>
-                <input
-                  v-model="formData.email"
-                  class="flex h-10 w-full flex-1 rounded-md file:border-0 border file:bg-transparent bg-white/20 px-4 py-3 text-sm file:text-sm file:font-medium text-white placeholder:text-gray-200 border-input ring-offset-background focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 focus-visible:ring-ring focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-                  type="email"
-                />
+                <p
+                  class="text-sm text-red-100"
+                  v-if="formData.username.dirty && !isUsernameValid"
+                >
+                  Username can only contain numbers and letters. It must be
+                  between 3 and 20 characters.
+                </p>
               </div>
             </div>
-            <div class="flex flex-col gap-4 md:flex-row">
-              <div class="flex w-full flex-col">
+            <div class="flex flex-col md:gap-4 md:flex-row">
+              <div class="w-full">
                 <label for="name" class="my-2 text-sm text-white"
                   >Password</label
                 >
                 <input
-                  v-model="formData.password"
+                  @input="() => markDirty('password')"
+                  v-model="formData.password.value"
                   class="flex h-10 w-full flex-1 rounded-md file:border-0 border file:bg-transparent bg-white/20 px-4 py-3 text-sm file:text-sm file:font-medium text-white placeholder:text-gray-200 border-input ring-offset-background focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 focus-visible:ring-ring focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                   type="password"
                 />
+                <p
+                  class="text-sm text-red-100"
+                  v-if="formData.password.dirty && !isPasswordLongEnough"
+                >
+                  Password must be at least 8 characters long.
+                </p>
               </div>
-              <div class="flex w-full flex-col">
+              <div class="w-full">
                 <label for="name" class="my-2 text-sm text-white"
                   >Confirm Password</label
                 >
                 <input
-                  v-model="formData.passwordConfirm"
+                  @input="() => markDirty('passwordConfirm')"
+                  v-model="formData.passwordConfirm.value"
                   class="flex h-10 w-full flex-1 rounded-md file:border-0 border file:bg-transparent bg-white/20 px-4 py-3 text-sm file:text-sm file:font-medium text-white placeholder:text-gray-200 border-input ring-offset-background focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 focus-visible:ring-ring focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                   type="password"
                 />
+                <p
+                  class="text-sm text-red-100"
+                  v-if="formData.passwordConfirm.dirty && !doPasswordsMatch"
+                >
+                  Passwords do not match.
+                </p>
               </div>
             </div>
             <button
+              :disabled="buttonDisabled"
               class="inline-flex w-full mt-4 items-center justify-center whitespace-nowrap text-sm ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 bg-white text-orange-600 hover:bg-gray-200 rounded-md py-3 px-6 font-medium"
             >
               Join Now
